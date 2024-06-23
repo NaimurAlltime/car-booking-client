0df99b44966d233bb2a3a18c7fc4b87de2632303
Naimur Rahman <naimur2231@gmail.com>
2024-06-23 22:40:20 +0600

    create middleware function manage private route

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,14 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 import { envConfig } from "./helpers/config/envConfig";
 
-// This function can be marked `async` if using `await` inside
+// This function can be marked `async` if using `await` inside middleware
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const session: any = await getToken({ req: request, secret: envConfig.jwt.secret });
-  
+  const session: any = await getToken({
+    req: request,
+    secret: envConfig.jwt.secret,
+  });
+
   if (!session) {
     return NextResponse.redirect(new URL("/admin-login", request.nextUrl));
   }
